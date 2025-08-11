f440d1f4d10ed2d8e7b0bf5d81de235e51a683f7
Xexer Ibrahimli <xezerib2008@gmail.com>
2025-08-11 04:37:30 +0400

    Add Info Div and Return Div finder

--- MyApp.js
+++ MyApp.js
@@ -1,6 +1,6 @@
 
 let allDivs = document.getElementById('allDivs')
-console.log(allDivs);
+console.log(allDivs.childNodes);
 
 function CreateNewDiw(idName , className){
     let newDiv = document.createElement('div')
@@ -14,15 +14,16 @@ function CreateNewDiw(idName , className){
     newDiv.style.display = "flex"
     newDiv.style.alignItems = "center"
     newDiv.style.justifyContent = "center";
-    document.body.appendChild(newDiv)
+    allDivs.appendChild(newDiv)
     return newDiv
+
 }
 
 function classDiv(classDivSet){
 
     classDivSet.style.minWidth = "500px";
     classDivSet.style.width = "100%";
-    classDivSet.style.height = "300px";
+    classDivSet.style.minHeight = "300px";
     classDivSet.style.backgroundColor = "#1e1e1e";
     classDivSet.style.borderRadius = "15px";
     classDivSet.style.border = "solid 1px whitesmoke"
@@ -31,17 +32,53 @@ function classDiv(classDivSet){
     classDivSet.style.boxShadow = "whitesmoke 0px 0px 10px"
     allDivs.appendChild(classDivSet)
     allDivs.appendChild(document.createElement('hr'))
+    
+    document.querySelectorAll(".infoDiv").forEach(target => infoDiv(target))
+    function infoDiv(infoDivSet){
+        
+        infoDivSet.style.minWidth = "225px";
+        infoDivSet.style.width = "50%";
+        infoDivSet.style.minHeight = "200px"
+        infoDivSet.style.height = "85%"
+        infoDivSet.style.margin = "10px"
+        infoDivSet.style.borderRadius = "20px"
+        infoDivSet.style.border = "whitesmoke 1px solid"
+        infoDivSet.style.boxShadow = "whitesmoke 0px 0px 5px"
+
+        if (classDivSet.id === infoDivSet.id) {
+            classDivSet.appendChild(infoDivSet)
+        }
+    }
+
+    document.querySelectorAll(".returnDiv").forEach(target => returnDiv(target))
+    function returnDiv(returnDivSet){
+        returnDivSet.style.minWidth = "225px";
+        returnDivSet.style.width = "50%";
+        returnDivSet.style.minHeight = "200px"
+        returnDivSet.style.height = "85%"
+        returnDivSet.style.margin = "10px"
+        returnDivSet.style.borderRadius = "20px"
+        returnDivSet.style.border = "whitesmoke 1px solid"
+        returnDivSet.style.boxShadow = "whitesmoke 0px 0px 5px"
+        returnDivSet.style.backgroundColor ="#0e0e0e"
+        if (classDivSet.id === returnDivSet.id) {
+            classDivSet.appendChild(returnDivSet)
+        }
+    }
 }
 
 // function checkClass(targetClass){if(targetClass.className === "classDiv") classDiv(targetClass)}
 
+let StrInfo = new CreateNewDiw("strMetods" , "infoDiv")
 let StrDiv = new CreateNewDiw("strMetods" , "classDiv")
 let NumDiv = new CreateNewDiw("numMetods","classDiv")
 let BoolDiv = new CreateNewDiw("boolMetods","classDiv")
 let ObjDiv = new CreateNewDiw("objMetods","classDiv")
 let ArrDiv = new CreateNewDiw("arrMetods","classDiv")
+let StrReturn = new CreateNewDiw("strMetods","returnDiv")
 
 document.querySelectorAll(".classDiv").forEach(target => classDiv(target))
+// document.querySelectorAll(".infoDiv").forEach(target => infoDiv(target))
 
 // checkClass(StrDiv)
 // checkClass(NumDiv)
