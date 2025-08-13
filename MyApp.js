1403eb083cc8ba79c047738b6a0d550017f8d471
Xexer Ibrahimli <xezerib2008@gmail.com>
2025-08-13 22:01:42 +0400

    I FInd a Path

--- MyApp.js
+++ MyApp.js
@@ -1,19 +1,45 @@
 const allDivs = document.getElementById("allDivs")
 
-let CreateElementFunc = function (Type , IdName , ClassName1 , MethodName) {
-    allDivs.append(document.createElement(Type))
+let CreateElementFunc = function (Type , IdName , ClassName , MethodName) {
+    let ElementTag = allDivs.appendChild(document.createElement(Type))
     let ElementsDiv = allDivs.querySelectorAll("div")
-    ElementsDiv.id = IdName
-    ElementsDiv.className = ClassName1
+    ElementTag.id = IdName
+    ElementTag.classList.add(ClassName,MethodName)
 }
+
+CreateElementFunc("div",'deneme','InfoDiv','deneme2')
+CreateElementFunc("div",'deneme','denem1','deneme2')
 CreateElementFunc("div",'deneme','InfoDiv','deneme2')
 CreateElementFunc("div",'deneme','denem1','deneme2')
 CreateElementFunc("div",'deneme','denem1','deneme2')
 CreateElementFunc("div",'deneme','denem1','deneme2')
 CreateElementFunc("div",'deneme','denem1','deneme2')
+CreateElementFunc("div",'deneme','denem1','deneme2')
+CreateElementFunc("div",'deneme','denem1','deneme2')
+CreateElementFunc("div",'deneme','InfoDiv','deneme2')
+CreateElementFunc("div",'deneme','InfoDiv','deneme2')
+CreateElementFunc("div",'deneme','InfoDiv','deneme2')
+CreateElementFunc("div",'deneme','InfoDiv','deneme2')
+CreateElementFunc("div",'deneme','InfoDiv','deneme2')
 
 // let ClassDiv = document.createElement('dic')
 // ClassDiv.className = "ClassDiv"
-console.log(allDivs.childNodes);
+// console.log(allDivs.childNodes);
+let Divs = allDivs.childNodes
+let sameID = new Set()
 
+Divs.forEach(element =>{
+    let classN = element.classList[0]
+    if(classN){
+        if (sameID.has(classN)) {
+            element.remove()
+        }else{
+            sameID.add(classN)
+        }
+    }
+    
+})
+console.log(sameID);
+
+console.log(Divs)
 // allDivs.append(ClassDiv)
