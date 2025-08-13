0623766288d62f5283b451b522183eef353012e6
Xexer Ibrahimli <xezerib2008@gmail.com>
2025-08-14 02:24:35 +0400

    I learn Find Function

--- MyApp.js
+++ MyApp.js
@@ -7,39 +7,42 @@ let CreateElementFunc = function (Type , IdName , ClassName , MethodName) {
     ElementTag.classList.add(ClassName,MethodName)
 }
 
-CreateElementFunc("div",'deneme','InfoDiv','deneme2')
-CreateElementFunc("div",'deneme','denem1','deneme2')
-CreateElementFunc("div",'deneme','InfoDiv','deneme2')
-CreateElementFunc("div",'deneme','denem1','deneme2')
-CreateElementFunc("div",'deneme','denem1','deneme2')
-CreateElementFunc("div",'deneme','denem1','deneme2')
-CreateElementFunc("div",'deneme','denem1','deneme2')
-CreateElementFunc("div",'deneme','denem1','deneme2')
-CreateElementFunc("div",'deneme','denem1','deneme2')
-CreateElementFunc("div",'deneme','InfoDiv','deneme2')
-CreateElementFunc("div",'deneme','InfoDiv','deneme2')
-CreateElementFunc("div",'deneme','InfoDiv','deneme2')
-CreateElementFunc("div",'deneme','InfoDiv','deneme2')
-CreateElementFunc("div",'deneme','InfoDiv','deneme2')
+CreateElementFunc("div",'String','InfoDiv','Trim')
+CreateElementFunc("div",'String','ClassDiv','Trim')
+CreateElementFunc("div",'String','InfoDiv','Splice')
+CreateElementFunc("div",'String','ReturDiv','Trim')
+CreateElementFunc("div",'Array','ReturnDiv','Push')
+CreateElementFunc("div",'Array','ClassDiv','Push')
+CreateElementFunc("div",'Array','InfoDiv','Push')
+ChekDivs()
 
 // let ClassDiv = document.createElement('dic')
 // ClassDiv.className = "ClassDiv"
 // console.log(allDivs.childNodes);
+function ChekDivs(){
 let Divs = allDivs.childNodes
 let sameID = new Set()
+let sameClass = new Set()
+let sameMethod = new Set()
 
 Divs.forEach(element =>{
-    let classN = element.classList[0]
-    if(classN){
-        if (sameID.has(classN)) {
+    let IdN = element.id
+    let ClassN = element.classList[0]
+    let MethodN = element.classList[1]
+
+    if(IdN && ClassN && MethodN){
+        
+        if (sameID.has(IdN) && sameClass.has(ClassN) && sameMethod.has(MethodN)) {
+            console.log(element);
             element.remove()
         }else{
-            sameID.add(classN)
+            sameID.add(IdN)
+            sameClass.add(ClassN)
+            sameMethod.add(MethodN)
         }
     }
     
 })
-console.log(sameID);
-
-console.log(Divs)
+console.log(sameID,sameClass,sameMethod);
+}
 // allDivs.append(ClassDiv)
