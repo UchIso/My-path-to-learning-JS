826ec9111107e01c8cc41fa24d1dfecf3b50e5ae
Xexer Ibrahimli <xezerib2008@gmail.com>
2025-08-14 19:24:50 +0400

    Litle update

--- MyApp.js
+++ MyApp.js
@@ -5,35 +5,40 @@ let CreateElementFunc = function (Type , IdName , ClassName , MethodName) {
     let ElementsDiv = allDivs.querySelectorAll("div")
     ElementTag.id = IdName
     ElementTag.classList.add(ClassName,MethodName)
+    if(ElementTag.classList[0] === "ClassDiv"){
+    allDivs.appendChild(document.createElement("hr"))
+}
 }
 
 CreateElementFunc("div",'String','InfoDiv','Trim')
 CreateElementFunc("div",'String','ClassDiv','Trim')
 CreateElementFunc("div",'String','InfoDiv','Splice')
-CreateElementFunc("div",'String','ReturDiv','Trim')
+CreateElementFunc("div",'String','ReturnDiv','Trim')
 CreateElementFunc("div",'Array','ReturnDiv','Push')
+CreateElementFunc("div",'Array','ClassDiv','Push')
+CreateElementFunc("div",'Array','ClassDiv','Push')
+CreateElementFunc("div",'Array','ClassDiv','Push')
 CreateElementFunc("div",'Array','ClassDiv','Push')
 CreateElementFunc("div",'Array','InfoDiv','Push')
 ChekDivs()
 
-// let ClassDiv = document.createElement('dic')
-// ClassDiv.className = "ClassDiv"
-// console.log(allDivs.childNodes);
+let ClassDiv = document.createElement('dic')
+ClassDiv.className = "ClassDiv"
+console.log(allDivs.childNodes);
+
 function ChekDivs(){
 let Divs = allDivs.childNodes
 let sameID = new Set()
 let sameClass = new Set()
 let sameMethod = new Set()
 
-Divs.forEach(element =>{
+Divs.forEach(element => {
     let IdN = element.id
     let ClassN = element.classList[0]
     let MethodN = element.classList[1]
 
     if(IdN && ClassN && MethodN){
-        
-        if (sameID.has(IdN) && sameClass.has(ClassN) && sameMethod.has(MethodN)) {
-            console.log(element);
+        if (sameClass.has(ClassN) && sameID.has(IdN) && sameMethod.has(MethodN)) {
             element.remove()
         }else{
             sameID.add(IdN)
@@ -43,6 +48,6 @@ Divs.forEach(element =>{
     }
     
 })
+console.log(allDivs.childNodes);
 console.log(sameID,sameClass,sameMethod);
 }
-// allDivs.append(ClassDiv)
