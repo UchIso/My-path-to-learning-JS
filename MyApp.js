1ee1ce13226c4d49d070d0e787f00a76774123ad
Xexer Ibrahimli <xezerib2008@gmail.com>
2025-08-16 15:39:45 +0400

    Update Css

--- MyApp.js
+++ MyApp.js
@@ -12,26 +12,25 @@ let CreateElementFunc = function (Type , IdName , ClassName , MethodName) {
         ClassHr.classList.add("ClassHr",MethodName)
     }else{
         Classifier(ElementTag)
-        ElementTag.remove()
     }
     ChekDivs(allDivs)
 }
 
 function ChekDivs(ParentElement){
+    
 
     const allElements = ParentElement.querySelectorAll('*');
     const SimpleDiv = new Set()
 
     allElements.forEach(element => {
 
-        const ID = element.id || ``;
+        const ID = element.id || `NoName`;
         const CLASS = Array.from(element.classList).sort().join(" ")
         const Personal = `${ID}_${CLASS}`
     
         if (SimpleDiv.has(Personal)) {
-
+            console.log("Bir Kopya bulundu. Imha ediliyor...");
             console.log(element);
-            console.log("Bir Kopya bulundu. Imha ediliyor");
             console.log(`ID --> "${ID}" || Class --> "${CLASS}"`)
             console.log("");
             
@@ -44,11 +43,22 @@ function ChekDivs(ParentElement){
 }
 
 function Classifier(Element){
+    
     let ClassDiv = allDivs.querySelectorAll(".ClassDiv")
-    // console.log(Element);
-    console.log(ClassDiv);
+    
+    ClassDiv.forEach(element => {
+        const id = element.id;
+        const method = Array.from(element.classList).sort()[1]
+        const eDone = (id + method)
+        const EDone = (Element.id + Element.classList[1])
         
+        if(eDone == EDone){
+            element.appendChild(Element)
+        }
+    })
+    
 }
+
 
 CreateElementFunc("div",'String','ClassDiv','Trim')
 CreateElementFunc("div",'String','InfoDiv','Trim')
