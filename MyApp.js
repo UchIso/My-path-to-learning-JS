9673840c3188341749460d0f90f5ee332862f934
Xexer Ibrahimli <xezerib2008@gmail.com>
2025-08-16 05:32:23 +0400

    update CSS and Class Finder

--- MyApp.js
+++ MyApp.js
@@ -5,49 +5,64 @@ let CreateElementFunc = function (Type , IdName , ClassName , MethodName) {
     ElementTag.id = IdName
     ElementTag.classList.add(ClassName,MethodName)
 
+    
     if(ElementTag.classList[0] === "ClassDiv"){
         let ClassHr = allDivs.appendChild(document.createElement("hr"))
         ClassHr.id = IdName;
         ClassHr.classList.add("ClassHr",MethodName)
+    }else{
+        Classifier(ElementTag)
+        ElementTag.remove()
     }
+    ChekDivs(allDivs)
 }
 
-CreateElementFunc("div",'String','InfoDiv','Trim')
+function ChekDivs(ParentElement){
+
+    const allElements = ParentElement.querySelectorAll('*');
+    const SimpleDiv = new Set()
+
+    allElements.forEach(element => {
+
+        const ID = element.id || ``;
+        const CLASS = Array.from(element.classList).sort().join(" ")
+        const Personal = `${ID}_${CLASS}`
+    
+        if (SimpleDiv.has(Personal)) {
+
+            console.log(element);
+            console.log("Bir Kopya bulundu. Imha ediliyor");
+            console.log(`ID --> "${ID}" || Class --> "${CLASS}"`)
+            console.log("");
+            
+            element.remove()
+        }else{
+            SimpleDiv.add(Personal)
+        }
+    
+    })
+}
+
+function Classifier(Element){
+    let ClassDiv = allDivs.querySelectorAll(".ClassDiv")
+    // console.log(Element);
+    console.log(ClassDiv);
+        
+}
+
 CreateElementFunc("div",'String','ClassDiv','Trim')
-CreateElementFunc("div",'String','ClassDiv','toString')
-CreateElementFunc("div",'String','InfoDiv','Splice')
+CreateElementFunc("div",'String','InfoDiv','Trim')
 CreateElementFunc("div",'String','ReturnDiv','Trim')
-CreateElementFunc("div",'Array','ReturnDiv','Push')
-CreateElementFunc("div",'Array','ClassDiv','Push')
+CreateElementFunc("div",'String','ClassDiv','toString')
+CreateElementFunc("div",'Array','ClassDiv','Push')  
 CreateElementFunc("div",'Array','InfoDiv','Push')
-ChekDivs(allDivs)
+CreateElementFunc("div",'Array','ReturnDiv','Push')
+// ChekDivs(allDivs)
 
 let ClassDiv = document.createElement('div')
 ClassDiv.className = "ClassDiv"
 
-console.log(allDivs.childNodes);
+// console.log(allDivs.childNodes);
 
-function ChekDivs(ParentElement){
-
-const allElements = ParentElement.querySelectorAll('*');
-
-const SimpleDiv = new Set()
-
-allElements.forEach(element => {
-
-    const ID = element.id || ``;
-    const CLASS = Array.from(element.classList).sort().join(" ")
-    const Personal = `${ID}_${CLASS}`
-    
-    if (SimpleDiv.has(Personal)) {
-        (function(){console.log(element)})()
-        console.log(`ID - "${ID}" ve Class - "${CLASS}" Bir Kopya bulundu. Imha ediliyor`)
-        element.remove()
-    }else{
-        SimpleDiv.add(Personal)
-    }
-    
-})
-}
 
 
